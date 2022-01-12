0c10726150a5f300398c1a3c53447d74de963fc2
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-01-12 12:26:54 -0600

    adds 3 products' names, price, image, description

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# product = Product.create(name: "dumbbells", price: 399.99, image_url: "https://post.healthline.com/wp-content/uploads/2020/10/458490-These-Are-the-12-Best-Dumbbells-to-Use-At-Home-According-to-A-Trainer_ProductShot_Yes4All-Adjustable-Dumbbells.png", description: "A free weight used during exercise or muscle-building. It comes in a variety of weights and sizes.")
+
+product = Product.create(name: "kettlebell", price: 85, image_url: "https://onnits3.imgix.net/authority/568.png?auto=format&w=735", description: "A large cast-iron ball-shaped weight with a single handle.")
+
+product = Product.create(name: "squat rack", price: 595, image_url: "https://www.yanrefitness.com/wp-content/uploads/2021/04/HM-6501-Squat-Stand-yanrefitness.jpg", description: "A metal rack used for supporting a barbell during heavy weightlifting exercises.")
