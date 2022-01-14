2b4e00181cd457e84cafb59bcbfee710934dc79a
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-01-14 11:34:30 -0600

    adds index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  def index
+    product = Product.all
+    render json: product
+  end
+
+  # def any_product
+  #   product_name = params[:id]
+  #   render json: { message: "Any product" }
+  # end
+
+  def show
+    id = params[:id]
+    product = Product.find(id: id)
+    render json: product
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/all_products" => "products#index"
+
+  get "/first_product", controller: "products", action: "first_product"
+
+  get "/any_product" => "products#show"
+
+  get "/any_product/:product" => "products#show"
 end
